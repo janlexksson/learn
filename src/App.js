7ef9adb5ec75d16195308a297ddce5b483903a98
janlexksson <105170783+janlexksson@users.noreply.github.com>
2022-09-09 14:06:28 +0200

    DarkTheme

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import Home from './components/Home'
+import React, {useState} from 'react';
+import styled, {ThemeProvider} from "styled-components";
+import {lightTheme, darkTheme, GlobalStyles} from "./themes.js";
+
+const StyledApp = styled.div`
+
+color: ${props => props.theme.fontColor};
+`;
 
 function App() {
+    const [theme, setTheme] = useState("light");
+
+
+    const themeToggler = () => {
+        theme === 'light' ? setTheme("dark") : setTheme("light"); 
+    }
     return(
+        <ThemeProvider theme={theme === "light" ? lightTheme  : darkTheme}>
+       <GlobalStyles/>
+       <StyledApp>
+       <button type="button" class="inline-block px-6 py-2.5 bg-red-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-red-700 hover:shadow-lg focus:bg-red-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-red-800 active:shadow-lg transition duration-150 ease-in-out"  onClick={() => themeToggler()}>Change Theme</button>
         <div>
             <Home />
         </div>
+        </StyledApp>
+        </ThemeProvider>
+      
     );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import {GoSmiley} from 'react-icons/go'
+import {VscSmiley} from 'react-icons/vsc'
 const Home = () => {
   return (
     <div className='with-screen h-screen'>
-        <div className='h-full flex flex-col justify-center items-center text-[59em] animate-spin-slow'>
+        <div className='z-10 h-full flex flex-col justify-center items-center text-[2em] animate-spin-slow'>
             <GoSmiley />
-        </div>
+            </div>
+            {/* <div className='h-full flex flex-col justify-right items-start text-[4em] animate-spin-slow'>
+            <VscSmiley />
+        </div> */}
     </div>
   )
 }
